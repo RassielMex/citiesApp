54db886d1b06cd3e7b6c3e73cbd9fd83541070d6
RassielMex <rhcp94.rm@gmail.com>
2022-10-25 23:06:37 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import { FormControl, InputLabel, MenuItem, Select } from "@mui/material";
+import { useState } from "react";
+import "./App.css";
+import Graph from "./components/Graph";
 
 function App() {
+  const data = {
+    cities: ["Merida", "Guadalajara", "Nuevo Leon"],
+    indicator1: [3, 4, 5],
+    indicator2: [5, 6, 7],
+  };
+
+  const [cityFilter, setCityFilter] = useState(0);
+  const [filteredData, setFilteredData] = useState(data);
+
+  const handleChange = (e) => {
+    const value = e.target.value;
+
+    if (value === 0) {
+      console.log("Default");
+      setFilteredData(data);
+    } else {
+      console.log("Other");
+      setFilteredData({
+        cities: [data.cities[value - 1]],
+        indicator1: [data.indicator1[value - 1]],
+        indicator2: [data.indicator2[value - 1]],
+      });
+    }
+
+    setCityFilter(value);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <FormControl fullWidth>
+        <InputLabel id="select-label">City</InputLabel>
+        <Select
+          labelId="select-label"
+          id="select-city"
+          label="City"
+          value={cityFilter}
+          onChange={handleChange}
         >
-          Learn React
-        </a>
-      </header>
+          <MenuItem value={0} selected>
+            Todos
+          </MenuItem>
+          {data?.cities?.map((city, index) => {
+            return (
+              <MenuItem key={index} value={index + 1}>
+                {city}
+              </MenuItem>
+            );
+          })}
+        </Select>
+      </FormControl>
+
+      <Graph data={filteredData} />
     </div>
   );
 }
---
+++ src/components/Graph.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Bar } from "react-chartjs-2";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+const Graph = ({ data }) => {
+  const { cities, indicator1, indicator2 } = data;
+
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "top",
+      },
+      title: {
+        display: true,
+        text: "Chart.js Bar Chart",
+      },
+    },
+  };
+
+  const graphData = {
+    labels: cities,
+    datasets: [
+      {
+        label: "N° de Servicios",
+        data: indicator1,
+        backgroundColor: "rgb(25, 99, 132)",
+      },
+      {
+        label: "Meta para la ciudad",
+        data: indicator2,
+        backgroundColor: "rgb(75, 192, 192)",
+      },
+    ],
+  };
+
+  return (
+    <div>
+      <Bar options={options} data={graphData} />;
+    </div>
+  );
+};
+
+export default Graph;
